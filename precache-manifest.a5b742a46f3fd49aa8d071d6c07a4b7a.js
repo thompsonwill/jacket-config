7684a063ca6fe165ec71d90b28cf64685311bf30
Will Thompson <williamjamesthompson@gmail.com>
2020-09-30 10:27:30 -0500

    Updates

---
+++ precache-manifest.a5b742a46f3fd49aa8d071d6c07a4b7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d0fe9d076f6e29ac1c297f00026670",
+    "url": "/jacket-config/index.html"
+  },
+  {
+    "revision": "c13bd32907c3c0a518ec",
+    "url": "/jacket-config/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "da54a18fff6fe7227098",
+    "url": "/jacket-config/static/css/main.3e41ded5.chunk.css"
+  },
+  {
+    "revision": "c13bd32907c3c0a518ec",
+    "url": "/jacket-config/static/js/2.eef60447.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jacket-config/static/js/2.eef60447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da54a18fff6fe7227098",
+    "url": "/jacket-config/static/js/main.f6c39921.chunk.js"
+  },
+  {
+    "revision": "b67a522d359d8637960e",
+    "url": "/jacket-config/static/js/runtime-main.9bd332df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jacket-config/static/media/logo.5d5d9eef.svg"
+  }
+]);
